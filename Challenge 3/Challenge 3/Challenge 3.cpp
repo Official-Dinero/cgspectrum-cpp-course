e838341560f2ee1436eed0d08e5f47073929050f
William Barnes <william.barnes578@outlook.com>
2023-10-21 16:44:56 +0100

    - initial commit

--- Challenge 3/Challenge 3/Challenge 3.cpp
+++ Challenge 3/Challenge 3/Challenge 3.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int CurrentYear;
+    cout << "Please enter the current year: ";
+    cin >> CurrentYear;
+
+    if (CurrentYear % 400 == 0 || (CurrentYear % 4 == 0 && CurrentYear % 100 != 0))
+    {
+        cout << "It is a Leap Year.";
+    }
+    else
+    {
+        cout << "It is not a Leap Year.";
+    }
 }
