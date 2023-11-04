37f34f4bdf885e9321278eb26e0beb4454438655
William Barnes <william.barnes578@outlook.com>
2023-11-04 16:42:51 +0000

    - Added Binary Search to end

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 
-int i, j, SizeOfArray, NumberList[100];
+int i, j, SizeOfArray, NumberList[100], NumberToFind;
 void QuickSort(int NumberList[], int SizeofArray);
 void PrintArray(int arr[], int SizeOfArr);
+int BinarySearch(int NumberList[], int left, int t, int right);
 
 int main()
 {
@@ -24,6 +25,13 @@ int main()
 
     cout << "\nArray after sorting: \n";
     PrintArray(NumberList, SizeOfArray);
+
+    cout << "\nEnter a Number to find in the Array: ";
+    cin >> NumberToFind;
+
+    int result = BinarySearch(NumberList, 0, NumberToFind, SizeOfArray - 1);
+
+    cout << "Your Element is at index: " << result;
 }
 
 
@@ -49,5 +57,29 @@ void PrintArray(int arr[], int SizeOfArr)
     for (i = 0; i < SizeOfArr; i++)
     {
         cout << arr[i] << " ";
+    }
+}
+
+int BinarySearch(int NumberList[], int left, int t, int right)
+{
+    while (left <= right)
+    {
+        int middle = (left + (right - 1)) / 2;
+
+        if (NumberList[middle] == t)
+        {
+            return middle;
+        }
+
+        if (NumberList[middle] < t)
+        {
+            left = middle + 1;
+        }
+
+        else
+        {
+            right = middle - 1;
+        }
+
     }
 }
